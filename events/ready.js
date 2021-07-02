ffa6fade03bbc8dcfdef5cb8ecdd7e6339443dee
MrTheBank <thebankzana@yahoo.com>
2021-07-02 10:49:58 +0700

    add discord activity

--- events/ready.js
+++ events/ready.js
@@ -4,5 +4,6 @@ module.exports = async (client) => {
     await DB.db_connect().then(() => {
         console.log('[MySQL]: Connection Successful.');
     });
+    await client.user.setActivity(process.env.DISCORD_PREFIX+'help | mrthebank.maxnus.com', { type: 'PLAYING'});
     console.log("[Discord API]: Logged In As " + client.user.tag);
 };
