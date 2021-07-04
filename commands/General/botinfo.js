582be36cc54bc82a74ea5a0f43961df671f16fbb
MrTheBank <thebankzana@yahoo.com>
2021-07-04 16:22:10 +0700

    fixed show users

--- commands/General/botinfo.js
+++ commands/General/botinfo.js
@@ -6,6 +6,8 @@ exports.run = async (client, message) => {
     const version = await fs.readFileSync('./version.txt', 'utf8');
     const uptime = ms.to(d,h,m,s)(Math.floor(process.uptime()*1000));
 
+    const users = message.client.guilds.cache.map((g) => g.memberCount).reduce((a, c) => a + c);
+
     await message.channel.send(
         new MessageEmbed()
             .setColor('#0099ff')
@@ -15,7 +17,7 @@ exports.run = async (client, message) => {
                 'ไอดี: `'+client.user.id+'`\n' +
                 'จำนวนเซิฟเวอร์: `'+client.guilds.cache.size+'`\n' +
                 'จำนวนช่อง: `'+client.channels.cache.size+'`\n' +
-                'จำนวนผู้ใช้งาน: `'+client.users.cache.size+'`\n' +
+                'จำนวนผู้ใช้งาน: `'+users+'`\n' +
                 'เวลาทำงาน: `'+uptime[0]+' วัน '+uptime[1]+' ชั่วโมง '+uptime[2]+' นาที '+uptime[3]+' วินาที`')
             .setFooter('เขียนด้วย discord.js • เวอร์ชั่น '+version)
     );
