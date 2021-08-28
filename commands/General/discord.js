bd63675320655627786c5dafd4bc3c74c7b5e457
MrTheBank <thebankzana@yahoo.com>
2021-08-28 20:44:38 +0700

    delete old

--- commands/General/discord.js
+++
@@ -1,3 +0,0 @@
-exports.run = async (client, message) => {
-    await message.channel.send('<a:discord_spinning:861131405534822401> เข้า MrTheBank\'s Official Discord:\nhttps://discord.gg/vJdWa57HbX');
-};
--- utils/is_numeric.js
+++
@@ -1,6 +0,0 @@
-function is_numeric(str) {
-    if (typeof str != 'string') return false
-    return !isNaN(str) && !isNaN(parseFloat(str))
-}
-
-module.exports = is_numeric;
